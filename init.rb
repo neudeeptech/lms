d1f8c091c11b872da14db2136082d683baac0e63
Someshwar Mirge <mirgesomeshwar@gmail.com>
2020-05-09 13:42:03 +0530

    basic directory structure generated

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :lms do
+  name 'Lms plugin'
+  author 'College Of Engineer ,Ambajogai'
+  description 'This is a leave management system plugin for redmine '
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
