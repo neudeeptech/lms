e6f29be356f65075d4cc458291287d09e7206d3d
Someshwar Mirge <mirgesomeshwar@gmail.com>
2020-07-20 09:45:45 +0530

    routes.rb file modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 get 'lms_controller',:to=>'lms_controller#lms_main'
-=======
->>>>>>> 6a465a76c0c49420d39b9c258327d19105982d06
+#=======
+#>>>>>>> 6a465a76c0c49420d39b9c258327d19105982d06
