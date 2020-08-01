25897e88e5b6daf00d8c7595439026ece945b87c
Someshwar Mirge <mirgesomeshwar@gmail.com>
2020-08-01 09:38:19 +0530

    edited init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :lms do
   name 'Lms plugin'
-#<<<<<<< HEAD
+
   author 'College Of Engineer ,Ambajogai'
   description 'This is a leave management system plugin for redmine '
   version '0.0.1'
